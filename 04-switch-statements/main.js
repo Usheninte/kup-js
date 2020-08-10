190219383e2d462d6bbc2afd21884ad786264556
Ninte <ninte.dev@gmail.com>
2020-08-10 20:25:37 +0000

    added type validity check to label inputs

--- 04-switch-statements/main.js
+++ 04-switch-statements/main.js
@@ -1,1 +1,35 @@
-console.log("Hello.")
+const assert = require('assert').strict;
+
+/*
+* The function should accept 4 parameters:
+*--
+* value1, label1, value2, label2.
+* * *
+* If the inputs are valid, it should return an array with 2 variables inside of it:
+* value3, and  label3. For example:
+*--
+* return [5,"minutes"];
+* */
+
+
+function timeAdder(value1, label1, value2, label2) {
+    let labels = [label1, label2];
+    // checking if label type is string
+    let labelTypeValidity = [];
+    labels.forEach(label => labelTypeValidity.push(typeof label == 'string'));
+    console.log(labelTypeValidity);
+    labelTypeValidity.forEach(label => {
+        if(label === false) {
+            throw "Error: Invalid input.";
+        }
+    });
+
+    // ...
+    console.log("! Final line.");
+}
+
+try {
+    timeAdder(1, "minute", 2, "seconds")
+} catch (e) {
+    console.error(e)
+}
