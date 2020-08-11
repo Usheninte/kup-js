472b028c96e16cd81f1e3ef7dafbb9a8cd9854fe
Ninte <ninte.dev@gmail.com>
2020-08-11 14:05:08 +0000

    completed time calcutation functionality, prepared file for submission

--- 04-switch-statements/main.js
+++ 04-switch-statements/main.js
@@ -1,18 +1,14 @@
-const assert = require('assert').strict;
-
-/*
-* The function should accept 4 parameters:
-*--
-* value1, label1, value2, label2.
-* * *
-* If the inputs are valid, it should return an array with 2 variables inside of it:
-* value3, and  label3. For example:
-*--
-* return [5,"minutes"];
-* */
-
-
 function timeAdder(value1, label1, value2, label2) {
+    /*
+    * The function should accept 4 parameters: value1, label1, value2, label2.
+    *
+    * If the inputs are valid, it should return an array
+    * with 2 variables inside of it - value3, and  label3.
+    * For example:
+    *
+    * return [5,"minutes"];
+    * */
+
     let values = [value1, value2];
     let labels = [label1, label2];
 
@@ -27,78 +23,137 @@ function timeAdder(value1, label1, value2, label2) {
 
     const labelWordsBucket = Object.values(labelWords);
 
-    // console.log(lableWordsBucket);
-
-    let clockBucket = []
+    let clockBucket = [];  // holder for valid value & label pairings
 
     function inputChecker(val, lab) {
 
         // checking values for type number
         val.forEach(value => {
             if (typeof value !== 'number') {
-                throw "Error ...";
+                // throw 'Error ...';
+                return false;
             }
-        })
+        });
 
         // checking labels for type string
         lab.forEach(label => {
             if (typeof label !== 'string') {
-                throw "Error ...";
+                // throw 'Error ...';
+                return false;
             };
 
             if (labelWordsBucket[0].includes(label) === false
                 && labelWordsBucket[1].includes(label) === false) {
-                throw "Error ...";
+                // throw 'Error ...';
+                return false;
             };
         });
 
         // checking valid value & label pairing (1)
         if (val[0] === 1 && singular.includes(lab[0])) {
 
-            const first_arg = [val[0], lab[0]];
-            clockBucket.push(first_arg);
+            const first_arg = [val[0], lab[0]];  // save valid value & label to array
+            clockBucket.push(first_arg);  // append array to cloudBucket
 
         } else if (val[0] > 1 && plural.includes(lab[0])) {
 
-            const first_arg = [val[0], lab[0]];
-            clockBucket.push(first_arg);
+            const first_arg = [val[0], lab[0]];  // save valid value & label to array
+            clockBucket.push(first_arg);  // append array to cloudBucket
 
         } else {
-            throw "Error ...";
-        }
+            // throw 'Error ...';
+            return false;
+        };
 
         // checking valid value & label pairing (2)
         if (val[1] === 1 && singular.includes(lab[1])) {
 
-            const second_arg = [val[1], lab[1]];
-            clockBucket.push(second_arg);
+            const second_arg = [val[1], lab[1]];  // save valid value & label to array
+            clockBucket.push(second_arg);  // append array to cloudBucket
 
         } else if (val[1] > 1 && plural.includes(lab[1])) {
 
-            const second_arg = [val[1], lab[1]];
-            clockBucket.push(second_arg);
+            const second_arg = [val[1], lab[1]];  // save valid value & label to array
+            clockBucket.push(second_arg);  // append array to cloudBucket
 
         } else {
-            throw "Error ...";
-        }
+            // throw 'Error ...';
+            return false;
+        };
 
-        console.log(clockBucket);
+        function timeCalc() {
+            // timing
+            const sec = 1;
+            const min = 60 * sec;
+            const hr = 60 * min;
+            const dy = 24 * hr;
+
+            // arguments
+            const arg1 = clockBucket[0];  // value & label pairing (1)
+            const arg2 = clockBucket[1];  // value & label pairing (2)
+
+            // conversion to seconds
+            let secondsBucket = [0, 0];  // base array to store conversions
+
+            // value & label pairing (1)
+            switch (arg1[1]) {
+                case 'second': case 'seconds':
+                    secondsBucket[0] += (arg1[0] * sec);  // conversion unchanged for seconds
+                    break;
+                case 'minute': case 'minutes':
+                    secondsBucket[0] += (arg1[0] * min);  // perform timing conversion
+                    break;
+                case 'hour': case 'hours':
+                    secondsBucket[0] += (arg1[0] * hr);  // perform timing conversion
+                    break;
+                case 'day': case 'days':
+                    secondsBucket[0] += (arg1[0] * dy);  // perform timing conversion
+                    break;
+                default:
+                    return false;
+            }
+
+            // value & label pairing (2)
+            switch (arg2[1]) {
+                case 'second': case 'seconds':
+                    secondsBucket[1] += (arg2[0] * sec);  // conversion unchanged for seconds
+                    break;
+                case 'minute': case 'minutes':
+                    secondsBucket[1] += (arg2[0] * min);  // perform timing conversion
+                    break;
+                case 'hour': case 'hours':
+                    secondsBucket[1] += (arg2[0] * hr);  // perform timing conversion
+                    break;
+                case 'day': case 'days':
+                    secondsBucket[1] += (arg2[0] * dy);  // perform timing conversion
+                    break;
+                default:
+                    return false;
+            }
+
+            // return result
+
+            const label3 = 'seconds';  // chosen label
+            const value3 = secondsBucket[0] + secondsBucket[1];  // sum total of conversion values
+
+            let result = [value3, label3];  // create array as specified in requirements
+            return result;
+
+        };
+
+        console.log(timeCalc());  // logs result of time calculation to console
 
     };
 
-    // function timeCalc(val, lab) {
-    // }
-
     inputChecker(values, labels);
-    // timeCalc(values, labels);
-
-    // ...
-    console.log("! Final line.");
 
 };
 
-try {
-    timeAdder(1, "minute", 2, "seconds");
-} catch (e) {
-    console.error(e);
-}
+// // development setup
+// try {
+//     timeAdder(2, 'days', 23, 'hours');
+// } catch (e) {
+//     console.error(e);
+// }
+
+timeAdder(3, 'days', 23, 'hours');
