35cadda2e76d4a719b2a9d0e33cd6cf4ca423f12
nullthefirst <ninte.dev@gmail.com>
2021-06-16 15:23:06 +0100

    Added conditional to print multiples of 5 as 'Buzz'.

--- 06-loops/index.js
+++ 06-loops/index.js
@@ -1,8 +1,12 @@
 // main for loop
 for (let i = 1; i <= 100; i++) {
-    // conditional to bring multiples of 3 as "Fizz"
+    // conditional to print multiples of 3 as "Fizz"
     if (i % 3 === 0) {
         console.log("Fizz");
+    }
+    // conditional to print multiples of 5 as "Buzz"
+    else if (i % 5 === 0) {
+        console.log("Buzz")
     }
     else {
         console.log(i);
