9f904339f288b2cd33e224efdefb718b399fa61f
nullthefirst <ninte.dev@gmail.com>
2021-06-16 15:21:35 +0100

    Added conditional to print multiples of 3 as 'Fizz'.

--- 06-loops/index.js
+++ 06-loops/index.js
@@ -1,4 +1,10 @@
-// Main for loop
+// main for loop
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    // conditional to bring multiples of 3 as "Fizz"
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(i);
+    }
 }
