7f08976450686025c46407e0baab40c6040323d4
nullthefirst <ninte.dev@gmail.com>
2021-06-20 15:34:05 +0100

    Added arrays holding win ID sequences.

--- homework/08-events/main.js
+++ homework/08-events/main.js
@@ -1,1 +1,18 @@
-alert('JS loaded');
+const horizontalWins = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
+];
+
+const verticalWins = [
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"]
+];
+
+const diagonalWins = [
+    ["1", "5", "9"],
+    ["3", "5", "7"]
+];
+
+console.log(horizontalWins, verticalWins, diagonalWins);
