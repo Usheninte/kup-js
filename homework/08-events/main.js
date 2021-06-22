258bad3f554daeb25a4e7fb7af2eb38303b92174
nullthefirst <ninte.dev@gmail.com>
2021-06-22 20:17:58 +0100

    Trimmed unused win rules.

--- homework/08-events/main.js
+++ homework/08-events/main.js
@@ -1,27 +1,3 @@
-// win rules
-const horizontalWins = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"]
-];
-
-const verticalWins = [
-    ["1", "4", "7"],
-    ["2", "5", "8"],
-    ["3", "6", "9"]
-];
-
-const diagonalWins = [
-    ["1", "5", "9"],
-    ["3", "5", "7"]
-];
-
-const winContext = {
-    'horizontal': horizontalWins,
-    // 'vertical': verticalWins,
-    // 'diagonal': diagonalWins
-}
-
 // DOM extraction
 
 const gameBoard = document.getElementById("game-board");
