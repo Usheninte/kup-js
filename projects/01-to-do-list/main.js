1330e461fdcd8e8efe3fa2fc627abc7e86016710
nullthefirst <ninte.dev@gmail.com>
2021-06-23 10:54:32 +0100

    Implemented view switch on signup button click.

--- projects/01-to-do-list/main.js
+++ projects/01-to-do-list/main.js
@@ -1,5 +1,13 @@
+// views
+const baseView = document.getElementById('base-view');
+const signupForm = document.getElementById('signup-form');
+
 // auth DOM extraction
 const signup = document.getElementById('sign-up');
 const login = document.getElementById('log-in');
 
 // sign up
+signup.addEventListener('click', () => {
+    baseView.classList.toggle('visually-hidden');
+    signupForm.classList.toggle('visually-hidden');
+});
