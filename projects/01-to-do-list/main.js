80ef80c13f84468f14ffc5ddf3129fb6b9942cfb
nullthefirst <ninte.dev@gmail.com>
2021-06-23 14:50:26 +0100

    Added error warnings display. Included logic for warning auto removal.

--- projects/01-to-do-list/main.js
+++ projects/01-to-do-list/main.js
@@ -1,13 +1,70 @@
 // views
-const baseView = document.getElementById('base-view');
-const signupForm = document.getElementById('signup-form');
+const baseView = document.getElementById('baseView');
+const signupForm = document.getElementById('signupForm');
 
-// auth DOM extraction
-const signup = document.getElementById('sign-up');
-const login = document.getElementById('log-in');
+// sign up DOM extraction
+const signup = document.getElementById('signup');
+const signupSubmit = document.getElementById('signupSubmit');
+// const login = document.getElementById('login');
 
 // sign up
 signup.addEventListener('click', () => {
     baseView.classList.toggle('visually-hidden');
     signupForm.classList.toggle('visually-hidden');
+});
+
+signupSubmit.addEventListener('click', (e) => {
+    // input DOM extraction
+    const regFirstName = document.getElementById('registerFirstName');
+    const regLastName = document.getElementById('registerLastName');
+    const regEmailAddress = document.getElementById('registerEmailAddress');
+    const regPassword = document.getElementById('registerPassword');
+    const regTerms = document.getElementById('registerTerms');
+
+    // input errors
+    const regFirstNameError = document.getElementById('registerFirstNameError');
+    const regLastNameError = document.getElementById('registerLastNameError');
+    const regEmailAddressError = document.getElementById('registerEmailAddressError');
+    const regPasswordError = document.getElementById('registerPasswordError');
+    const regTermsError = document.getElementById('registerTermsError');
+
+    if (!regFirstName.value) {
+        regFirstNameError.innerText = 'First name can not be blank';
+        regFirstName.parentElement.appendChild(regFirstNameError);
+        setTimeout(() => regFirstNameError.innerText = '', 3000);
+    }
+
+    if (!regLastName.value) {
+        regLastNameError.innerText = 'Last name can not be blank';
+        regLastName.parentElement.appendChild(regLastNameError);
+        setTimeout(() => regLastNameError.innerText = '', 3000);
+    }
+
+    if (!regEmailAddress.value) {
+        regEmailAddressError.innerText = 'Email address can not be blank';
+        regEmailAddress.parentElement.appendChild(regEmailAddressError);
+        setTimeout(() => regEmailAddressError.innerText = '', 3000);
+    }
+
+    if (!regPassword.value) {
+        regPasswordError.innerText = 'Password can not be blank';
+        regPassword.parentElement.appendChild(regPasswordError);
+        setTimeout(() => regPasswordError.innerText = '', 3000);
+    }
+
+    if (regTerms.checked !== true) {
+        regTermsError.innerText = 'Terms should not be blank';
+        regTerms.parentElement.appendChild(regTermsError);
+        setTimeout(() => regTermsError.innerText = '', 3000);
+    }
+
+    e.preventDefault();
+    const user = {
+        'firstName': regFirstName.value,
+        'lastName': regLastName.value,
+        'email': regEmailAddress.value,
+        'password': regPassword.value,
+        'terms': regTerms.checked
+    };
+    console.log(user);
 });
