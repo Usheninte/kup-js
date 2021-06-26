47cf1601161100055b6a4dfa81685c8b5c317198
nullthefirst <ninte.dev@gmail.com>
2021-06-26 11:41:27 +0100

    Displayed update form on account settings.

--- projects/01-to-do-list/main.js
+++ projects/01-to-do-list/main.js
@@ -10,6 +10,7 @@ const content = document.getElementById('content');
 const authBar = document.getElementById('authBar');
 const logout = document.getElementById('logout');
 const accountSettings = document.getElementById('accountSettings');
+const updateForm = document.getElementById('updateForm');
 
 // empty field check
 const emptyFieldCheck = (inputElement, inputElementError, errorTextName) => {
@@ -117,6 +118,16 @@ const registerUser = (userInformation) => {
     database.setItem(`${userInformation['email']}`, JSON.stringify(userInformation));
 }
 
+// update user in local storage
+
+const updateUser = (userInformation) => {
+    // const userInfoBase = database.getItem(`${userInformation['email']}`);
+
+    // const userInformationValues = JSON.parse(userInfoBase);
+    database.setItem(`${userInformation['email']}`, json.stringify(userInformation));
+}
+
+// sign up flow
 signup.addEventListener('click', () => {
     baseView.classList.toggle('visually-hidden');
     signupForm.classList.toggle('visually-hidden');
@@ -145,13 +156,13 @@ signupSubmit.addEventListener('click', (e) => {
 
     // form field validations
     emptyFieldCheck(regFirstName, regFirstNameError, 'First name');
-    valueLengthCheck(regFirstName, registerFirstNameLengthError, 3, 'First name');
+    valueLengthCheck(regFirstName, regFirstNameLengthError, 3, 'First name');
     emptyFieldCheck(regLastName, regLastNameError, 'Last name');
-    valueLengthCheck(regLastName, registerLastNameLengthError, 3, 'Last name');
+    valueLengthCheck(regLastName, regLastNameLengthError, 3, 'Last name');
     emptyFieldCheck(regEmailAddress, regEmailAddressError, 'Email address');
-    valueLengthCheck(regEmailAddress, registerEmailAddressLengthError, 6, 'Email address');
+    valueLengthCheck(regEmailAddress, regEmailAddressLengthError, 6, 'Email address');
     generalValidation(
-        validateEmail(regEmailAddress.value), registerEmailAddressContentError,
+        validateEmail(regEmailAddress.value), regEmailAddressContentError,
         'Email address should be in valid format');
     emptyFieldCheck(regPassword, regPasswordError, 'Password');
     valueLengthCheck(regPassword, regPasswordLengthError, 6, 'Password');
@@ -159,13 +170,13 @@ signupSubmit.addEventListener('click', (e) => {
 
     const inputValid = fullyValidForm(
         emptyFieldCheck(regFirstName, regFirstNameError, 'First name'),
-        valueLengthCheck(regFirstName, registerFirstNameLengthError, 3, 'First name'),
+        valueLengthCheck(regFirstName, regFirstNameLengthError, 3, 'First name'),
         emptyFieldCheck(regLastName, regLastNameError, 'Last name'),
-        valueLengthCheck(regLastName, registerLastNameLengthError, 3, 'Last name'),
+        valueLengthCheck(regLastName, regLastNameLengthError, 3, 'Last name'),
         emptyFieldCheck(regEmailAddress, regEmailAddressError, 'Email address'),
-        valueLengthCheck(regEmailAddress, registerEmailAddressLengthError, 6, 'Email address'),
+        valueLengthCheck(regEmailAddress, regEmailAddressLengthError, 6, 'Email address'),
         generalValidation(
-            validateEmail(regEmailAddress.value), registerEmailAddressContentError,
+            validateEmail(regEmailAddress.value), regEmailAddressContentError,
             'Email address should be in valid format',
         ),
         emptyFieldCheck(regPassword, regPasswordError, 'Password'),
@@ -214,7 +225,7 @@ login.addEventListener('click', () => {
     loginForm.classList.toggle('visually-hidden');
 });
 
-// behaviour on sign up click
+// behaviour on log in click
 loginSubmit.addEventListener('click', (e) => {
     // input DOM extraction
     const loginEmailAddress = document.getElementById('loginEmailAddress');
@@ -285,11 +296,91 @@ logout.addEventListener('click', () => {
     loginPassword.value = '';
 });
 
-// // account settings flow
-// accountSettings.addEventListener('click', () => {
-//     // remove dashboard and authBar
-//     dashboard.classList.toggle('visually-hidden');
-//     authBar.classList.toggle('visually-hidden');
+// account settings flow
+accountSettings.addEventListener('click', () => {
+    // remove dashboard and authBar
+    dashboard.classList.toggle('visually-hidden');
+    authBar.classList.toggle('visually-hidden');
+    updateForm.classList.toggle('visually-hidden');
 
+    // input DOM extraction
+    const updateFirstName = document.getElementById('updateFirstName');
+    const updateLastName = document.getElementById('updateLastName');
+    const updateEmailAddress = document.getElementById('updateEmailAddress');
+    const updatePassword = document.getElementById('updatePassword');
+    const updateTerms = document.getElementById('updateTerms');
 
-// });
+    updateFirstName.value = regFirstName.value;
+    updateLastName.value = regLastName.value;
+    updateEmailAddress.value = regEmailAddress.value;
+    updatePassword.value = regPassword.value;
+    updateTerms.value = regTerms.value;
+
+    // input errors DOM extraction
+    const updateFirstNameError = document.getElementById('updateFirstNameError');
+    const updateFirstNameLengthError = document.getElementById('updateFirstNameLengthError');
+    const updateLastNameError = document.getElementById('updateLastNameError');
+    const updateLastNameLengthError = document.getElementById('updateLastNameLengthError');
+    const updateEmailAddressError = document.getElementById('updateEmailAddressError');
+    const updateEmailAddressLengthError = document.getElementById('updateEmailAddressLengthError');
+    const updateEmailAddressContentError = document.getElementById('updateEmailAddressContentError');
+    const updatePasswordError = document.getElementById('updatePasswordError');
+    const updatePasswordLengthError = document.getElementById('updatePasswordLengthError');
+    const updateTermsError = document.getElementById('updateTermsError');
+
+    // form field validations
+    emptyFieldCheck(updateFirstName, updateFirstNameError, 'First name');
+    valueLengthCheck(updateFirstName, updateFirstNameLengthError, 3, 'First name');
+    emptyFieldCheck(updateLastName, updateLastNameError, 'Last name');
+    valueLengthCheck(updateLastName, updateLastNameLengthError, 3, 'Last name');
+    emptyFieldCheck(updateEmailAddress, updateEmailAddressError, 'Email address');
+    valueLengthCheck(updateEmailAddress, updateEmailAddressLengthError, 6, 'Email address');
+    generalValidation(
+        validateEmail(updateEmailAddress.value), updateEmailAddressContentError,
+        'Email address should be in valid format');
+    emptyFieldCheck(updatePassword, updatePasswordError, 'Password');
+    valueLengthCheck(updatePassword, updatePasswordLengthError, 6, 'Password');
+    generalValidation(updateTerms.checked, updateTermsError, 'Terms should not be blank');
+
+    const inputValid = fullyValidForm(
+        emptyFieldCheck(updateFirstName, updateFirstNameError, 'First name'),
+        valueLengthCheck(updateFirstName, updateFirstNameLengthError, 3, 'First name'),
+        emptyFieldCheck(updateLastName, updateLastNameError, 'Last name'),
+        valueLengthCheck(updateLastName, updateLastNameLengthError, 3, 'Last name'),
+        emptyFieldCheck(updateEmailAddress, updateEmailAddressError, 'Email address'),
+        valueLengthCheck(updateEmailAddress, updateEmailAddressLengthError, 6, 'Email address'),
+        generalValidation(
+            validateEmail(updateEmailAddress.value), updateEmailAddressContentError,
+            'Email address should be in valid format',
+        ),
+        emptyFieldCheck(updatePassword, updatePasswordError, 'Password'),
+        valueLengthCheck(updatePassword, updatePasswordLengthError, 6, 'Password'),
+        generalValidation(updateTerms.checked, updateTermsError, 'Terms should not be blank')
+    );
+
+    if (inputValid) {
+        const userInfo = {
+            'firstName': updateFirstName.value,
+            'lastName': updateLastName.value,
+            'email': updateEmailAddress.value,
+            'password': updatePassword.value,
+            'terms': updateTerms.checked,
+            'lists': []
+        };
+
+        // register user
+        updateUser(userInfo);
+
+        // switch to dashboard
+        if (loginValidation(userInfo, userInfo['password'], loginAuthenticationError,
+            'User with password entered does not exist')) {
+            // switch to dashboard
+            switchToDashboard(userInfo['email'], signupForm);
+        } else {
+            loginValidation(userInfo, userInfo['password'], loginAuthenticationError,
+                'User with password entered does not exist');
+        }
+
+        console.log(userInfo);
+    }
+});
