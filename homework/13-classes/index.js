04a25d37f51e36d2469065e465a35a3b5a7a1348
nullthefirst <ninte.dev@gmail.com>
2021-07-08 08:39:31 +0100

    Created base vehicle class.

--- homework/13-classes/index.js
+++ homework/13-classes/index.js
@@ -1,1 +1,24 @@
 // Classes
+
+class Vehicle {
+    constructor(needsMaintenance = false, tripsSinceMaintenance = 0) {
+        this.needsMaintenance = needsMaintenance;
+        this.tripsSinceMaintenance = tripsSinceMaintenance;
+    }
+
+    setMake(vehicleMake) {
+        this.vehicleMake = vehicleMake;
+    }
+
+    setModel(vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    setYear(vehicleYear) {
+        this.vehicleYear = vehicleYear;
+    }
+
+    setWeight(vehicleWeight) {
+        this.vehicleWeight = vehicleWeight;
+    }
+}
