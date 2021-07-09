1914fd132589ddad01f5debe58e8740ece88beff
nullthefirst <ninte.dev@gmail.com>
2021-07-09 08:40:43 +0100

    Created elevator class.

---
+++ projects/03-elevator/index.js
@@ -0,0 +1,7 @@
+class Elevator {
+    constructor(name, lowestFloor, highestFloor) {
+        this.name = name;
+        this.lowestFloor = lowestFloor;
+        this.highestFloor = highestFloor;
+    }
+}
